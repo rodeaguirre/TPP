c64fae184e10d2e5f90080f931f2fb8c7ccaa674
rodeaguirre <rocio.de.aguirre@gmail.com>
2023-09-18 16:43:16 -0300

    Cambiar directorios

--- sources/video/predict_tflite.py
+++ sources/video/predict_tflite.py
@@ -2,51 +2,39 @@ import tensorflow as tf
 import tflite_runtime.interpreter as tflite
 # from tensorflow.lite.python.interpreter import OpResolverType
 from tflite_functions import DataGenerator_tflite
-# from tensorflow.keras.mixed_precision import global_policy, set_global_policy, Policy
-# import traceback
-# policy = global_policy()
-# if policy.name == 'float32':
-#     policy = Policy('mixed_float16')
-# set_global_policy(policy)
+
 
 model = "model.tflite"
 npy_dir = "../../media/Images/NPY"
 
-batch_size = 2
+batch_size = 3
 dataset = 'ViolentFlow-opt'
-
-interprete = tf.lite.Interpreter(model_path=model, experimental_preserve_all_tensors=True)
-
-#interprete = Interpreter(model_path=model) # ,
-#                                 model_content=None,
-#                                 experimental_delegates=None,
-#                                 num_threads=1,
-#                                 experimental_op_resolver_type=OpResolverType.AUTO,
-#                                 experimental_preserve_all_tensors=False)
+interprete = tf.lite.Interpreter(model)
 
 input_shape = interprete.get_input_details()[0]['shape']
+
+# print(input_shape)
 interprete.resize_tensor_input(0, input_shape, strict=True)
+interprete.allocate_tensors()
 
 input_details = interprete.get_input_details()[0]
 output_details = interprete.get_output_details()[0]
-print(input_details)
-print(output_details)
 
-interprete.allocate_tensors()
 
 # Generate input
 input_model = DataGenerator_tflite(directory=npy_dir.format(dataset),
                                    batch_size_data=batch_size,
                                    data_augmentation=False)
-
 batch_x, batch_y = input_model.__getitem__(0)  # Use index 0 to get the first batch
+print(batch_x.shape)
 
-interprete.set_tensor(input_details['index'], batch_x)
 
-interprete.invoke()
-# delta_time = time() - time_before
-# print("Tiempo de predicción: ", delta_time)
+for i in range(batch_size):
+    input_ = batch_x[i:i+1]
+    print(input_.shape)
+    input_.reshape(input_shape)
+    interprete.set_tensor(input_details['index'], input_)
+    interprete.invoke()
+    predictions = interprete.get_tensor(output_details['index'])
+    print(predictions)
 
-predictions = interprete.get_tensor(output_details['index'])
-
-print(predictions)
