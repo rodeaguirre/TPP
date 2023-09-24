c48bf539e8e3d3093171f3d7b8bf7bad0a9697fe
rodeaguirre <rocio.de.aguirre@gmail.com>
2023-09-24 01:09:33 -0300

    Adapt get_video.py

--- sources/video/get_video.py
+++ sources/video/get_video.py
@@ -5,6 +5,8 @@ def get_video():
     subprocess.run(comando_video_1, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     comando_video_2 = "ffmpeg -i ../../media/Images/AVI/video.h264 -c:v mpeg4 -b:v 4000k ../../media/Images/AVI/video.avi"
     subprocess.run(comando_video_2, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    comando_video_3 = "rm ../../media/Images/AVI/video.h264"
+    subprocess.run(comando_video_3, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
 def remove_avi():
     comando_remove = "rm ../../media/Images/AVI/video.avi"
