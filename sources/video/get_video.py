48a0e9750abcfbff506038ec9e9f6835fcc04b6b
rodeaguirre <rocio.de.aguirre@gmail.com>
2023-09-24 01:07:47 -0300

    Adapt get_video.py

--- sources/video/get_video.py
+++ sources/video/get_video.py
@@ -1,10 +1,10 @@
 import subprocess
 
 def get_video():
-    comando_video = "libcamera-vid -o ../../media/Images/AVI/video.h264 -t 5000 --width 224 --height 224 --framerate 60"
-    subprocess.run(comando_video, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-    comando_video = "ffmpeg -i video.h264 -c:v mpeg4 -b:v 4000k video.avi"
-    subprocess.run(comando_video, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    comando_video_1 = "libcamera-vid -o ../../media/Images/AVI/video.h264 -t 5000 --width 224 --height 224 --framerate 60"
+    subprocess.run(comando_video_1, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    comando_video_2 = "ffmpeg -i ../../media/Images/AVI/video.h264 -c:v mpeg4 -b:v 4000k ../../media/Images/AVI/video.avi"
+    subprocess.run(comando_video_2, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
 def remove_avi():
     comando_remove = "rm ../../media/Images/AVI/video.avi"
