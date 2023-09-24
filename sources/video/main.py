a42dde15a27a575ea39e4665c5d48a4909c2d5f7
rodeaguirre <rocio.de.aguirre@gmail.com>
2023-09-24 01:14:56 -0300

    Agregar prints

--- sources/video/main.py
+++ sources/video/main.py
@@ -6,8 +6,13 @@ model = "model.tflite"
 avi_dir = '../../media/Images/AVI'
 npy_dir = "../../media/Images/NPY"
 
+print("get_video")
 get_video()
+print("Save2Npy")
 Save2Npy(file_dir=avi_dir, save_dir=npy_dir)
+print("predict_model")
 predictions = predict_model(model, npy_dir)
+print("remove_avi")
 remove_avi()
+print("remove_npy")
 remove_npy()
