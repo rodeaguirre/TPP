3ca9b38ad986f3e7b7424176eb362c04fe4392f6
rodeaguirre <rocio.de.aguirre@gmail.com>
2023-09-25 16:22:34 -0300

    add pico_i2c.py

--- sources/video/get_video.py
+++ sources/video/get_video.py
@@ -8,10 +8,13 @@ def get_video():
     if os.path.exists("../../media/Images/AVI/video.h264"):
         comando_video_2 = "ffmpeg -i ../../media/Images/AVI/video.h264 -c:v mpeg4 -b:v 4000k ../../media/Images/AVI/video.avi"
         subprocess.run(comando_video_2, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-        print("Comando 2")
-        comando_video_3 = "rm ../../media/Images/AVI/video.h264"
-        subprocess.run(comando_video_3, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-        print("Comando 3")
+        if os.path.exists("../../media/Images/AVI/video.avi"):
+            comando_video_3 = "y"
+            subprocess.run(comando_video_3, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+            print("Comando 3")
+        comando_video_4 = "rm ../../media/Images/AVI/video.h264"
+        subprocess.run(comando_video_4, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        print("Comando 4")
 
 def remove_avi():
     comando_remove = "rm ../../media/Images/AVI/video.avi"
--- sources/video/multi_thread.py
+++ sources/video/multi_thread.py
@@ -1,29 +1,64 @@
+import threading
+import time
+import os
+
 from get_video import get_video, remove_avi, remove_npy
 from predict_tflite import predict_model
 from preprocess_video import Save2Npy
-import threading
 
 RGB_model = "model.tflite"
 avi_dir = '../../media/Images/AVI'
 npy_dir = "../../media/Images/NPY"
 
-
-def captura_video(origin_dir, destin_dir):
-    get_video()
-    Save2Npy(file_dir=origin_dir, save_dir=destin_dir)
-    pass
-
-
-def inferencia_red(origin_dir, destin_dir, model):
-    prediction = predict_model(model, npy_dir)
+def capture_video(max_files, avi_dir):
+    i = 1
     while True:
-        # Llama a la función de inferencia en el hilo/proceso secundario
-        threading.Thread(target=captura_video, args=(origin_dir, destin_dir)).start()
+
+        # Create a text file with a name based on the index
+        with open(f"{avi_dir}/archivo{i}.txt", "w") as file:
+            file.write(f"Contenido del archivo {i}")
+        print(f"archivo{i}.txt creado.")
+
+        i += 1
+        if i > max_files:
+            i = 1  # Reset the counter if it exceeds max_files
+
+        time.sleep(30)
 
 
-captura_video(avi_dir, npy_dir)
+def predict_video(max_files, processing_files):
+    while True:
+        time.sleep(3)  # Simulate processing time
 
-# Mantener el programa principal en ejecución
-while True:
-    inferencia_red(avi_dir, npy_dir, RGB_model)
-    pass
+        # Find the latest file to process
+        latest_file_index = max_files
+        while not os.path.exists(f"files/archivo{latest_file_index}.txt") and latest_file_index > 1:
+            latest_file_index -= 1
+
+        if latest_file_index > 1:
+            ultimo_archivo = f"files/archivo{latest_file_index}.txt"
+            print(f"Procesando archivo {ultimo_archivo}...")
+
+            # Track the file being processed
+            processing_files.add(ultimo_archivo)
+
+            # Delete files that are not being processed
+            for filename in os.listdir("files"):
+                filepath = os.path.join("files", filename)
+                if filepath != ultimo_archivo and os.path.isfile(filepath) and filepath not in processing_files:
+                    os.remove(filepath)
+                    print(f"Archivo {filepath} eliminado.")
+
+            # Remove the processed file from the set
+            processing_files.remove(ultimo_archivo)
+
+
+if __name__ == "__main__":
+    max_files = 3
+    processing_files = set()
+
+    # Start the file creation thread
+    threading.Thread(target=capture_video, args=(max_files,avi_dir)).start()
+
+    # Start the file processing thread
+    threading.Thread(target=predict_video, args=(max_files, processing_files)).start()
