e9cd84cee72732dbded6bdc0bdb2b36733986d9e
rodeaguirre <rocio.de.aguirre@gmail.com>
2023-12-24 00:30:25 -0300

    Pi_UART envio y recibo - mensajes de debugg

--- sources/pi_UART.py
+++ sources/pi_UART.py
@@ -15,14 +15,18 @@ try:
             break
         ser.write(mensaje.encode())
 
+        mensaje = input("Mansaje enviado.")
         # Lectura por UART
         datos_recibidos = ser.readline().decode().strip()
+        if datos_recibidos:
+            mensaje = input("Mansaje enviado.")
+            # Imprime los datos recibidos
+            print("Datos recibidos:", datos_recibidos)
 
-        # Imprime los datos recibidos
-        print("Datos recibidos:", datos_recibidos)
-
-        # Envía los datos recibidos de vuelta
-        ser.write(datos_recibidos.encode())
+            # Envía los datos recibidos de vuelta
+            ser.write(datos_recibidos.encode())
+        else:
+            print("No se recibió nada")
 
 except KeyboardInterrupt:
     # Maneja la interrupción del teclado (Ctrl+C)
